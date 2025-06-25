812d30e4d072b0a8f982bca0668e551fa441c5cf
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-06-25 11:59:57 -0300

    fix: validation schemas

--- front-end/src/schemas/task-schema.ts
+++ front-end/src/schemas/task-schema.ts
@@ -3,6 +3,7 @@ import z from "zod";
 export const taskSchema = z.object({
   task: z
     .string()
+    .trim()
     .min(3, "Informe pelo menos 3 caracteres")
     .max(300, "Máximo de 300 caracteres permitido"),
   isPublic: z.boolean(),
