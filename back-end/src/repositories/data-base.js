5e3edc4cdf344321a474d63a593c0b6b2caee55f
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-07-08 21:14:28 -0300

    ops: add ssl to pool

--- back-end/src/repositories/data-base.js
+++ back-end/src/repositories/data-base.js
@@ -6,4 +6,7 @@ export const pool = new pg.Pool({
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
