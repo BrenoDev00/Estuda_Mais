a58b95cf0bceaabd86a168fe29e4d8fb6a9da595
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-07-09 18:31:32 -0300

    refactor: allowed origins

--- back-end/src/utils/constants/allowed-cors-origins.js
+++ back-end/src/utils/constants/allowed-cors-origins.js
@@ -1,4 +1,4 @@
 export const allowedOrigins = [
-  "https://estuda-mais-git-main-brenos-projects-d9dde81c.vercel.app/",
+  "https://estuda-mais-git-main-brenos-projects-d9dde81c.vercel.app",
   "http://localhost:3000",
 ];
